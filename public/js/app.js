0038076bb4cbc79478167a67cb36dbab812a1869
spacejack <spacejack@gmail.com>
2019-07-13 18:06:10 -0400

    Code edits for clarity

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,31 @@
+//@ts-check
+
 import * as THREE from './vendor/three.module.js'
 
-let container
-let camera, scene, renderer
+/**
+ * HTML elements
+ * @type {{container: HTMLElement | undefined; info: HTMLElement | undefined}}
+ */
+const dom = {container: undefined, info: undefined}
+/** @type {THREE.OrthographicCamera} */
+let camera
+/** @type {*} */
+let scene
+/** @type {THREE.WebGLRenderer} */
+let renderer
 
+/** Values that are passed to shader every render */
 const uniforms = {
 	time: {value: 1},
-	resolution: {value: [640, 480]}
+	resolution: {value: [window.innerWidth, window.innerHeight]}
 }
 
-function init (vertexShader, fragmentShader) {
-	container = document.getElementById('container')
+/**
+ * Initialize THREE Renderer and Scene
+ * @param {string} vertexShader
+ * @param {string} fragmentShader
+ */
+function initRenderer (vertexShader, fragmentShader) {
 	camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
 	scene = new THREE.Scene()
 
@@ -22,7 +38,7 @@ function init (vertexShader, fragmentShader) {
 
 	renderer = new THREE.WebGLRenderer()
 	renderer.setPixelRatio(window.devicePixelRatio)
-	container.appendChild(renderer.domElement)
+	dom.container.appendChild(renderer.domElement)
 
 	onWindowResize()
 	window.addEventListener('resize', onWindowResize, false)
@@ -35,22 +51,45 @@ function onWindowResize() {
 	uniforms.resolution.value = [w, h]
 }
 
+/**
+ * Start the animation loop
+ * @param {number} timestamp
+ */
 function animate (timestamp) {
 	uniforms.time.value = timestamp / 1000
 	renderer.render(scene, camera)
 	requestAnimationFrame(animate)
 }
 
-async function run() {
-	let r = await fetch('./shader/vertex.glsl' + '?t=' + Date.now()) // (appends cache-buster)
-	const vshader = await r.text()
-	r = await fetch('./shader/fragment.glsl' + '?t=' + Date.now())
-	const fshader = await r.text()
-	init(vshader, fshader)
+/** @param {Response} r */
+function decodeResponse (r) {
+	return r.status >= 200 && r.status < 400 ? r.text()
+		: Promise.reject(new Error(`HTTP error ${r.status}: ${r.url}`))
 }
 
-run().then(() => {
+/** Load the shader sources, then set up the THREE Renderer */
+async function init() {
+	// Collect some needed DOM elements
+	dom.container = document.querySelector('.container')
+	if (dom.container == null) {
+		throw new Error('.container element not found in dom')
+	}
+	dom.info = document.querySelector('.info')
+	// cache buster value
+	const t = Date.now()
+	const [vshader, fshader] = await Promise.all([
+		// append a cache-buster query string
+		fetch(`./shader/vertex.glsl?t=${t}`).then(decodeResponse),
+		fetch(`./shader/fragment.glsl?t=${t}`).then(decodeResponse)
+	])
+	initRenderer(vshader, fshader)
+}
+
+init().then(() => {
 	requestAnimationFrame(animate)
 }).catch(err => {
+	if (dom.info != null) {
+		dom.info.textContent = err.message
+	}
 	console.error(err.stack)
 })
--- readme.md
+++ readme.md
@@ -9,3 +9,5 @@
     npm start
 
 Then visit `http://localhost:3000/` in your browser.
+
+You can edit the shader sources in `public/shader`.
